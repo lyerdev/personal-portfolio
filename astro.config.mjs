1e53d508ca681b4eac8944164e8f5fa6e71bbc30
lyerdev <xerotimex@gmail.com>
2025-12-22 12:55:10 -0500

    Index update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import '/src/custom.css';
 
 // https://astro.build/config
 export default defineConfig({
