e55cae937494710ddbac983d02893aa061929c84
lyerdev <xerotimex@gmail.com>
2025-12-22 12:49:10 -0500

    Config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import '/src/custom.css';
 
 // https://astro.build/config
 export default defineConfig({
