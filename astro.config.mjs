28a2f5fe3a997a305da885bc6760c1de9041d0e0
lyerdev <xerotimex@gmail.com>
2025-12-22 11:21:18 -0500

    config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://lyerdev.github.io',
+	base: '/personal-portfolio',
 	integrations: [
 		starlight({
 			title: 'Lyle Zucker',
